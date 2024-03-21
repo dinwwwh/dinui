8d5ed9fd6841bcfb6d337ec5fdb962d865099e00
Din <dinsterizer@gmail.com>
2024-03-21 15:21:51 +0700

    wip

---
+++ apps/web/main.tsx
@@ -0,0 +1,19 @@
+import { MainLayout } from './layouts/main.tsx'
+import { ViteReactSSG } from 'vite-react-ssg'
+import type { RouteRecord } from 'vite-react-ssg'
+
+export const routes: RouteRecord[] = [
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        lazy: () => import('./pages/home.tsx'),
+        entry: './pages/home.tsx',
+      },
+    ],
+  },
+]
+
+export const createRoot = ViteReactSSG({ routes })
