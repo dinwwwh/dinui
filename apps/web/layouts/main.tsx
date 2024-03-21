c6c2ae0d1e12087ef2fff456cdc17bc87bd18cf8
Din <dinsterizer@gmail.com>
2024-03-21 16:42:00 +0700

    improve head tags

--- apps/web/layouts/main.tsx
+++ apps/web/layouts/main.tsx
@@ -1,7 +1,24 @@
 import { Outlet } from 'react-router-dom'
+import { Head } from 'vite-react-ssg'
 
 export function MainLayout() {
-  return <Outlet />
+  return (
+    <>
+      <Head>
+        <title>DinUI</title>
+        <meta
+          name="description"
+          content="Untitled UI react components build on top of TailwindCSS"
+        />
+        <meta
+          name="keywords"
+          content="React, React UI Components, Untitled UI, Untitled UI React, React UI Kits"
+        />
+        <meta name="author" content="Din" />
+      </Head>
+      <Outlet />
+    </>
+  )
 }
 
 export function MainErrorBoundary() {
