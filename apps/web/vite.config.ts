d83917399de8ec716b7c0801ca2a784b377a68f9
Din <dinsterizer@gmail.com>
2024-03-22 09:54:29 +0700

    improve example like real usage

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@web': resolve(__dirname, './'),
-      '@react-ui': resolve(__dirname, '../../packages/react-ui'),
+      '@dinui/react': resolve(__dirname, '../../packages/react-ui/ui'),
     },
   },
 })
