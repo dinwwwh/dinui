585a84e30212d74c4bb5fc55e69f00bdd4e1a5e0
Din <dinsterizer@gmail.com>
2024-03-22 09:42:05 +0700

    example component

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-  content: ['./layouts/**.tsx', './pages/**.tsx', './components/**.tsx'],
+  content: ['./layouts/**.tsx', './pages/**.tsx', './components/**.tsx', './examples/**.tsx'],
   theme: {
     extend: {},
   },
