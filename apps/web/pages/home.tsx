a35d911567c8ae9c117d692f7f4d75b9b47f7624
Din <dinsterizer@gmail.com>
2024-04-02 15:51:45 +0700

    home page

--- apps/web/pages/home.tsx
+++ apps/web/pages/home.tsx
@@ -1,10 +1,39 @@
-import { Button } from '@dinui/react/button'
+import { Github } from '@web/components/icons'
+import {
+  PageActions,
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading,
+} from '@web/components/page-header'
+import { buttonVariants } from '@web/components/ui/button'
+import { env } from '@web/env'
+import { cn } from '@web/lib/utils'
+import { Link } from 'vite-react-ssg'
 
 export function Component() {
   return (
-    <div className="text-sm py-10">
-      <span className="text-utility-blue-700 text-gray divide-primary/">home page</span>
-      <Button>xin chaa</Button>
+    <div className="container relative">
+      <PageHeader>
+        <PageHeaderHeading>Build your component library</PageHeaderHeading>
+        <PageHeaderDescription>
+          Beautifully designed components that you can copy and paste into your apps. Accessible.
+          Customizable. Open Source.
+        </PageHeaderDescription>
+        <PageActions>
+          <Link to="/docs" className={cn(buttonVariants())}>
+            Get Started
+          </Link>
+          <a
+            href={env.GITHUB_REPOSITORY_URL}
+            target="_blank"
+            rel="noreferrer"
+            className={cn(buttonVariants({ variant: 'outline' }))}
+          >
+            <Github className="mr-2 h-4 w-4" />
+            GitHub
+          </a>
+        </PageActions>
+      </PageHeader>
     </div>
   )
 }
