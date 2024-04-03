0e8ee7b4ba4e33bde3ffe556df0419a661d7ac3e
Din <dinsterizer@gmail.com>
2024-04-03 10:11:09 +0700

    tailwindcss

---
+++ apps/web/tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{html,md,mdx,tsx,js,jsx}',
+    './components/**/*.{html,md,mdx,tsx,js,jsx}',
+    './layout.tsx',
+    './styles.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
