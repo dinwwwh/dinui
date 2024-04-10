dc2dcd740497434e64eacfcd53b5412ddcfb5250
Din <dinsterizer@gmail.com>
2024-04-10 18:17:59 +0700

    fix version

--- packages/cli/utils/package.ts
+++ packages/cli/utils/package.ts
@@ -3,7 +3,7 @@ import path from 'node:path'
 import { type PackageJson } from 'type-fest'
 
 export function getPackageInfo() {
-  const packageJsonPath = path.join('package.json')
+  const packageJsonPath = path.resolve(__dirname /** from ./dist folder */, '../package.json')
 
   return fs.readJson(packageJsonPath) as Promise<PackageJson>
 }
