8ee512c85b648aa1f7e057e40f3a83a1981cb53f
Din <dinsterizer@gmail.com>
2024-04-11 15:49:01 +0700

    docs

--- apps/web/components/component-installation-guide.tsx
+++ apps/web/components/component-installation-guide.tsx
@@ -5,6 +5,7 @@ import {
 } from './copy-paste-component-code'
 import { Steps } from './steps'
 import { Tabs } from './tabs'
+import { UnifiedPackageManager } from './unified-package-manager'
 import { cn } from '@dinui/react/utils'
 import { Link } from 'react-router-dom'
 
@@ -18,27 +19,44 @@ export function ComponentInstallationGuide({ path, ...props }: Props) {
       <Tabs defaultValue="default" className="relative mr-auto w-full">
         <Tabs.List>
           <Tabs.List.Trigger value="default">Default</Tabs.List.Trigger>
-          <Tabs.List.Trigger value="cli" disabled>
-            CLI
-          </Tabs.List.Trigger>
+          <Tabs.List.Trigger value="cli">CLI</Tabs.List.Trigger>
           <Tabs.List.Trigger value="manual">Manual</Tabs.List.Trigger>
         </Tabs.List>
         <Tabs.Content value="default">
-          <InstallationNote />
+          <Steps>
+            <GlobalStartStep />
 
-          {props.children ? <Steps>{props.children}</Steps> : null}
+            {props.children}
+            <GlobalEndStep />
+          </Steps>
+        </Tabs.Content>
+        <Tabs.Content value="cli">
+          <Steps>
+            <GlobalStartStep />
+
+            <Steps.Heading>Run the following command in your project</Steps.Heading>
+            <UnifiedPackageManager
+              action="dlx"
+              name={`@dinui/cli@latest add ${path.replace(/(.\/)?ui\//, '')} -d ./components/ui`}
+            />
+
+            <Steps.Heading>Update the import paths to match your project setup</Steps.Heading>
+            {props.children}
+            <GlobalEndStep />
+          </Steps>
         </Tabs.Content>
         <Tabs.Content value="manual">
-          <InstallationNote />
-
           <Steps>
+            <GlobalStartStep />
+
             <Steps.Heading>Install dependencies</Steps.Heading>
             <ComponentDependenciesInstallation path={path} />
             <Steps.Heading>Copy and paste the following code into your project</Steps.Heading>
             <CopyPasteComponentCode path={path} />
-            <Steps.Heading>Update the import paths to match your project setup</Steps.Heading>
-
             {props.children}
+
+            <Steps.Heading>Update the import paths to match your project setup</Steps.Heading>
+            <GlobalEndStep />
           </Steps>
         </Tabs.Content>
       </Tabs>
@@ -46,17 +64,29 @@ export function ComponentInstallationGuide({ path, ...props }: Props) {
   )
 }
 
-function InstallationNote() {
+function GlobalStartStep() {
   return (
-    <Callout type="note">
-      You need to follow the instructions in the{' '}
-      <Link
-        to={'/installation'}
-        className="text-brand-600 hover:text-brand-700 dark:text-brand-400 dark:hover:text-brand-400/80"
-      >
-        Installation Instructions
-      </Link>{' '}
-      section to use it.
-    </Callout>
+    <>
+      <Steps.Heading>
+        Follow{' '}
+        <Link
+          to={'/installation'}
+          className="text-brand-600 hover:text-brand-700 dark:text-brand-400 dark:hover:text-brand-400/80"
+        >
+          Installation Guide
+        </Link>{' '}
+      </Steps.Heading>
+      To enable DinUI functionality in your project, you will need to properly set up Tailwind and
+      install the necessary dependencies.
+    </>
+  )
+}
+
+function GlobalEndStep() {
+  return (
+    <>
+      <Steps.Heading>All done</Steps.Heading>
+      You now can start using this component in your project.
+    </>
   )
 }
