43da4f9dd7731e12054a915a098ce84e198be03b
dinwwwh <dinwwwh@gmail.com>
2024-04-29 10:31:48 +0700

    hover card

--- apps/web/src/examples/ui/hover-card/demo.tsx
+++ apps/web/src/examples/ui/hover-card/demo.tsx
@@ -1,16 +1,16 @@
 import Avatar from '@dinui/react/avatar'
 import Button from '@dinui/react/button'
-import { HoverCard, HoverCardContent, HoverCardTrigger } from '@dinui/react/hover-card'
-import { CalendarIcon } from '@radix-ui/react-icons'
+import HoverCard from '@dinui/react/hover-card'
+import { IconCalendar } from '@tabler/icons-react'
 
 export default function HoverCardDemo() {
   return (
-    <HoverCard>
-      <HoverCardTrigger asChild>
+    <HoverCard open>
+      <HoverCard.Trigger asChild>
         <Button variant="ghost">@nextjs</Button>
-      </HoverCardTrigger>
-      <HoverCardContent className="w-80">
-        <div className="flex justify-between space-x-4">
+      </HoverCard.Trigger>
+      <HoverCard.Content className="w-80">
+        <div className="flex justify-between gap-4">
           <Avatar>
             <Avatar.Image src="https://github.com/vercel.png" />
             <Avatar.Fallback>VC</Avatar.Fallback>
@@ -18,13 +18,13 @@ export default function HoverCardDemo() {
           <div className="space-y-1">
             <h4 className="text-sm font-semibold">@nextjs</h4>
             <p className="text-sm">The React Framework – created and maintained by @vercel.</p>
-            <div className="flex items-center pt-2">
-              <CalendarIcon className="mr-2 h-4 w-4 opacity-70" />{' '}
-              <span className="text-xs text-wgray-500">Joined December 2021</span>
+            <div className="flex items-center pt-2 text-fg-weaker">
+              <IconCalendar className="mr-2 h-4 w-4" />{' '}
+              <span className="text-xs">Joined December 2021</span>
             </div>
           </div>
         </div>
-      </HoverCardContent>
+      </HoverCard.Content>
     </HoverCard>
   )
 }
--- packages/react-ui/src/ui/hover-card.tsx
+++ packages/react-ui/src/ui/hover-card.tsx
@@ -1,26 +1,42 @@
 'use client'
 
 import * as HoverCardPrimitive from '@radix-ui/react-hover-card'
-import * as React from 'react'
-import { twMerge } from 'tailwind-merge'
+import { forwardRef } from 'react'
+import { tv } from 'tailwind-variants'
 
-export const HoverCard = HoverCardPrimitive.Root
+const hoverCard = tv({
+  slots: {
+    content: [
+      'z-50 rounded-md border bg-bg--contrast p-4 shadow-md',
+      'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+      'data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2',
+      'data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+    ],
+  },
+})
 
-export const HoverCardTrigger = HoverCardPrimitive.Trigger
-
-export const HoverCardContent = React.forwardRef<
+const HoverCardContent = forwardRef<
   React.ElementRef<typeof HoverCardPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof HoverCardPrimitive.Content>
->(({ className, align = 'center', sideOffset = 4, ...props }, ref) => (
-  <HoverCardPrimitive.Content
-    ref={ref}
-    align={align}
-    sideOffset={sideOffset}
-    className={twMerge(
-      'z-50 w-64 rounded-md border border-wgray-200 dark:border-wgray-800 bg-white p-4 text-wgray-950 shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2  dark:bg-wgray-950 dark:text-wgray-50',
-      className,
-    )}
-    {...props}
-  />
-))
+>((props, ref) => {
+  const { content } = hoverCard()
+
+  return (
+    <HoverCardPrimitive.Content
+      align={'center'}
+      sideOffset={4}
+      {...props}
+      ref={ref}
+      className={content({ className: props.className })}
+    />
+  )
+})
 HoverCardContent.displayName = HoverCardPrimitive.Content.displayName
+
+const HoverCard = Object.assign(HoverCardPrimitive.Root, {
+  Trigger: HoverCardPrimitive.Trigger,
+  Content: HoverCardContent,
+})
+
+export default HoverCard
+export { hoverCard, HoverCardPrimitive }
